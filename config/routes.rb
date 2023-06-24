748ba318a9fbdd3b61b64c0eea9a645ecc355b52
Klaudia Biala <klaudia.biala@polcode.net>
2023-06-24 10:33:27 +0000

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :entries
+  resources :entries do
+    get 'increase_score', on: :member
+    get 'descrease_score', on: :member 
+  end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
