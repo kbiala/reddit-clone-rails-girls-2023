f736f35b784148f78d7970c503dd93322455d2a8
Klaudia Biala <klaudia.biala@polcode.net>
2023-06-24 09:00:26 +0000

    Create entries

---
+++ db/migrate/20230624083048_create_entries.rb
@@ -0,0 +1,11 @@
+class CreateEntries < ActiveRecord::Migration[7.0]
+  def change
+    create_table :entries do |t|
+      t.string :title
+      t.text :content
+      t.string :tag
+
+      t.timestamps
+    end
+  end
+end
