8e28ac584402543525f1d6062a25e1c62bc8e3dd
Klaudia Biala <klaudia.biala@polcode.net>
2023-06-24 13:39:13 +0000

    Add images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_24_100211) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_24_133856) do
   create_table "entries", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_24_100211) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "score", default: 0
+    t.string "image"
   end
 
 end
