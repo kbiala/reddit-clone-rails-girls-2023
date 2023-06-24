0f38e8f00036069919ad46cc4baa170bbb2f8aff
Klaudia Biala <klaudia.biala@polcode.net>
2023-06-24 11:03:42 +0000

    Connect view with js controller

--- app/javascript/controllers/scores_controller.js
+++ app/javascript/controllers/scores_controller.js
@@ -2,15 +2,29 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   // method will be here
+  static values = { id: Number }
+  static targets = [ "sum" ]
+
   up(e) {
     e.preventDefault();
     e.stopImmediatePropagation();
     console.log("arrow up is clicked");
+
+    let id = this.idValue;
+    let score = this.sumTarget;
+    console.log( id );
+    console.log( score );
+
+
   }
 
   down(e) {
     e.preventDefault();
     e.stopImmediatePropagation();
     console.log("arrow down is clicked");
+
+    let id = this.idValue;
+    let score = this.sumTarget;
+
   }
 }
